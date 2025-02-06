7421d8cde3450dfdae4526ba7a784f8b56f463d5
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-02-06 10:24:22 +0100

    Ejercicio 5

--- Main.java
+++ Main.java
@@ -37,5 +37,26 @@ public class Main {
             Coche.menu();
             opcion = scan.nextByte();
         }
+
+        //Ej 5
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length();
+        char[] tempCharArray = new char[len];
+        char[] charArray = new char[len];
+
+        for (int i = 0; i < len; i++) {
+            tempCharArray[i] = palindrome.charAt(i);
+        }
+
+        for (int j = 0; j < len; j++) {
+            charArray[j] = tempCharArray[len - 1 - j];
+        }
+
+        String reversePalindrome = new String(charArray);
+        System.out.println(reversePalindrome);
+
+        StringBuilder sb = new StringBuilder(palindrome);
+        reversePalindrome = sb.reverse().toString();
+        System.out.println(reversePalindrome);
     }
 }
